095e7be2f1ae993be24a12c9f329ace7fdcf4137
kruulxd <kruulxd@users.noreply.github.com>
2026-04-25 12:55:20 +0200

    Bump version to 1.2.1 to force Tampermonkey update

--- rzabiak-pack.user.js
+++ rzabiak-pack.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         RzabiakPack - Panel Dodatkow
 // @namespace    https://margonem.pl/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Loader panelu RzabiakPack
 // @author       kruulxd
 // @match        *://*.margonem.pl/*
